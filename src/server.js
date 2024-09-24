d506b49a5f534a9bb8342f03586783efe5c4a214
Niralak025 <niralak025@gmail.com>
2024-09-24 22:40:23 +0530

    ref - solved bugs for showing views

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path');
 
 // Middleware to parse JSON data
 app.use(express.json({ limit: '50mb' }));
@@ -18,14 +19,16 @@ app.use(cors())
 app.set('view engine', 'ejs');
 
 // Specify the directory where the view templates are stored
-app.set('views', './views');
+// app.set('views', './views');
+
+app.set('views', path.join(__dirname, 'views'));
 
 // Example of a static file middleware (for serving CSS, JS, etc.)
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 8080;
 
-const API_CALL = require('../src/routes/Router')
+const API_CALL = require('../src/routes/Router');
 
 app.use('/api', API_CALL)
 
